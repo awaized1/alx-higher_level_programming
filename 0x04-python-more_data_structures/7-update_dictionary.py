e8a6106902423c67da66c9273aa16e1815716fd6
awaized1 <aduolaayomide@gmail.com>
2023-09-14 00:38:22 +0000

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds value pairs to dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
