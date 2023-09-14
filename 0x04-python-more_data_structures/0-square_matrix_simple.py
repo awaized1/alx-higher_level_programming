8f40831cc42c073a8690321f3d9fe71fe763cfce
awaized1 <aduolaayomide@gmail.com>
2023-09-14 00:09:16 +0000

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that computes the square value ofa matrix"""
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
