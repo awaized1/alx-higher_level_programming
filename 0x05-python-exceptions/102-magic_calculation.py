19caa32a51f8f96d17fde74f90a4bac0b3250c1e
awaized1 <aduolaayomide@gmail.com>
2023-09-25 15:39:02 +0000

    updated

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-  """Code does a calculation on a and b
 
-  Args:
-     a: int
-     b: int
-
-  Returns:
-      Result of the claculation.
-  """
 
     res = 0
     for i in range(1, 3):
