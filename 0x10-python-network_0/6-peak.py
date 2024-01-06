78c23680436924f58afd98a7e491ca43816b49d0
awaized1 <aduolaayomide@gmail.com>
2024-01-05 19:28:41 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,22 @@
 
 
 def find_peak(list_of_integers):
-    """Function finds peak in list_of_integers""" 
+    """Function finds peak in list_of_integers"""
 
-    if list_of_integers is None or list_of_integers == []:
-        return None
-    low = 0
-    hi = len(list_of_integers)
-    mid = ((hi - low) // 2) + low
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    loy = 0
+    hi = len(list_of_integers)
+    mid = ((hi - loy) // 2) + loy
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
